7a223248dc3fd7aa96e710f270a3ef1f905daeba
Abdul_Malik <malik.info28@gmail.com>
2025-05-09 17:49:03 +0530

    added UI

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { RecipeItemComponent } from "./recipe-item/recipe-item.component";
+import { Recipe } from '../recipe.modal';
+import { RecipeDetailComponent } from "../recipe-detail/recipe-detail.component";
 
 @Component({
   selector: 'app-recipe-list',
-  imports: [],
+  imports: [RecipeItemComponent],
   templateUrl: './recipe-list.component.html',
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe('r1', 'A Test Recipe', 'This is a test', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/coconut-squash-dhansak-818351d.jpg?quality=90&resize=500,454'),
+    new Recipe('r2', 'A Test Recipe', 'This is a test', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/coconut-squash-dhansak-818351d.jpg?quality=90&resize=500,454'),
+  ];
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { ShopingEditComponent } from "./shoping-edit/shoping-edit.component";
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
-  imports: [],
+  imports: [ShopingEditComponent],
   templateUrl: './shopping-list.component.html',
   styleUrl: './shopping-list.component.css'
 })
 export class ShoppingListComponent {
+  ingredients: Ingredient[] = [
+    {
+      id:'i1',
+      name: 'Apples',
+      amount: 5
+    },
+    {
+      id:'i2',
+      name: 'Tomatoes',
+      amount: 10
+    },
+  ];
 
 }
