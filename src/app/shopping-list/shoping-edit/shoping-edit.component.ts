672b4be1ce28a851afaa3d5e7b0a4362ff7c0e71
Abdul_Malik <malik.info28@gmail.com>
2025-05-09 16:39:57 +0530

    addedd components

---
+++ src/app/shopping-list/shoping-edit/shoping-edit.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shoping-edit',
+  imports: [],
+  templateUrl: './shoping-edit.component.html',
+  styleUrl: './shoping-edit.component.css'
+})
+export class ShopingEditComponent {
+
+}
