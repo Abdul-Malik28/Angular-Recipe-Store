a71cac9f0125396519022706c8a9a64d087a6465
Abdul_Malik <malik.info28@gmail.com>
2025-05-10 01:11:27 +0530

    added directive to dropdown

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,9 +1,11 @@
 import { Component, Input } from '@angular/core';
+
 import { Recipe } from '../recipe.modal';
+import { DropdownDirective } from '../../shared/dropdown.directive';
 
 @Component({
   selector: 'app-recipe-detail',
-  imports: [],
+  imports: [DropdownDirective],
   templateUrl: './recipe-detail.component.html',
   styleUrl: './recipe-detail.component.css'
 })
