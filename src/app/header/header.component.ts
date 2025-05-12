195aef65b1919efd8b41b22dcde8bbc506f8b9fa
Abdul_Malik <malik.info28@gmail.com>
2025-05-12 11:51:37 +0530

    navigated through links

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,11 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
 import { DropdownDirective } from '../shared/dropdown.directive';
 
 @Component({
   selector: 'app-header',
-  imports: [DropdownDirective],
+  imports: [DropdownDirective, RouterLink, RouterLinkActive],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
