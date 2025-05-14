4b96377205e7b3cb2e4aecda99380f1956884cfa
Abdul_Malik <malik.info28@gmail.com>
2025-05-14 22:04:54 +0530

    added auth component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,12 @@ import { Routes } from '@angular/router';
 import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { routes as recipeRoutes } from './recipes/recipe.routes';
+import { AuthComponent } from './auth/auth/auth.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/recipes', pathMatch: 'full' },
     { path: 'recipes', component: RecipesComponent, children: recipeRoutes },
     { path: 'shopping-list', component: ShoppingListComponent },
+    { path: 'auth', component: AuthComponent }
+    // { path: '**', redirectTo: 'recipes', pathMatch: 'full' }
 ];
