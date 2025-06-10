c899151c0380e2a28c1e90fa2f1758449260c20e
Abdul_Malik <malik.info28@gmail.com>
2025-06-10 23:02:30 +0530

    registers the reducer

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,11 +5,14 @@ import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './auth/auth.interceptor';
 import { provideStore } from '@ngrx/store';
+import { shoppingListReducer } from './shopping-list/store/shopping-list.reducer';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes, withComponentInputBinding()),
     provideHttpClient(withInterceptors([authInterceptor])),
-    provideStore()
-]
+    provideStore({
+      shoppingList: shoppingListReducer,
+    })
+  ]
 };
