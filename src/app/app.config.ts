8dc908fc6435c8f1670a3827544e320d8e8df276
Abdul_Malik <malik.info28@gmail.com>
2025-06-11 20:49:29 +0530

    added stoe-devtools

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { provideStore } from '@ngrx/store';
 
@@ -8,12 +8,14 @@ import { authInterceptor } from './auth/auth.interceptor';
 import * as fromApp from './store/app.reducer';
 import { provideEffects } from '@ngrx/effects';
 import { AuthEffects } from './auth/store/auth.effects';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes, withComponentInputBinding()),
     provideHttpClient(withInterceptors([authInterceptor])),
     provideStore(fromApp.appReducers),
-    provideEffects([AuthEffects])
+    provideEffects([AuthEffects]),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
 ]
 };
