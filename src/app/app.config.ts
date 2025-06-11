854bea74c88ecea5bddc2d23e0d06181fc8ab58e
Abdul_Malik <malik.info28@gmail.com>
2025-06-11 17:06:10 +0530

    installed effects

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,11 +6,13 @@ import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './auth/auth.interceptor';
 import * as fromApp from './store/app.reducer';
+import { provideEffects } from '@ngrx/effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes, withComponentInputBinding()),
     provideHttpClient(withInterceptors([authInterceptor])),
-    provideStore(fromApp.appReducers)
-  ]
+    provideStore(fromApp.appReducers),
+    provideEffects()
+]
 };
